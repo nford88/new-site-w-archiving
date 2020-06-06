ff83986fd5bd8088ac27832d24e44f39b1167355
Nick Ford <nickford@MacBook-Pro.local>
2020-06-06 23:26:31 +0100

    create dist folder

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 var express = require('express');
 var server = express();
-server.use('/', express.static(__dirname + '/public/archive'));
+server.use('/', express.static(__dirname + '/src/archive'));
 server.listen(8080);
