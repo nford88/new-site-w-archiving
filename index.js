d363336f40864c4825c5dd41663f3a189e89b546
Nick Ford <nickford@MacBook-Pro.local>
2020-06-07 00:13:18 +0100

    remove dir name

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 var express = require('express');
 var server = express();
-server.use('/', express.static(__dirname + '/src/archive'));
+server.use('/', express.static('/src/archive'));
 server.listen(8080);
